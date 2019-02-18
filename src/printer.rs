c9ffe523b3fc3783d319881a27e64a4c9f0917fd
Adolfo Ochagavía <adolfo@infi.nl>
2019-02-18 17:28:35 +0100

    Add space invader

--- src/printer.rs
+++ src/printer.rs
@@ -4,6 +4,7 @@ use escposify::img::Image;
 
 pub struct Printer {
   logo: Image,
+  space_invader: Image,
   printer: escposify::printer::Printer<escposify::device::File::<File>>,
 }
 
@@ -12,7 +13,8 @@ impl Printer {
     let printer_file = escposify::device::File::<File>::new("\\\\ADOLFO\\BONNETJES");
 
     Printer {
-      logo: Image::new("infi2.bmp"),
+      logo: Image::new("infi.bmp"),
+      space_invader: Image::new("space-invader.bmp"),
       printer: escposify::printer::Printer::new(printer_file, None, None),
     }
   }
@@ -26,14 +28,20 @@ impl Printer {
     .bit_image(&self.logo, None)
     .line_space(-1)
     .text("----------------------------------")
-    .text("|                                |")
     .text("|          SPACE INVADERS        |")
-    .text("|                                |")
     .text("----------------------------------")
+    .text("")
+    .text("Nickname: Angry Darth Vader")
+    .text("")
     .text("Stats:")
-    .text("* Armor: 50")
-    .text("* Health: 20")
-    .text("Test")
+    .text("* Rate of fire: 5 bullets per second")
+    .text("* Shield strength: 10 hits")
+    .text("* Defense thickness: 10 pixels")
+    .text("* Dodging chance: 20%")
+    .text("")
+    .text("Good luck!")
+    .bit_image(&self.space_invader, None)
+    .line_space(-1)
     .feed(2)
     .cut(false)
     .flush()
